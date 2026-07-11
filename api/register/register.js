b85ba4fd9c71e7e738566df47ba4f92b1c753119
jjonas <joaosoares3798@gmail.com>
2026-07-11 01:29:07 -0300

    mudando .then para async/await

--- api/register/register.js
+++ api/register/register.js
@@ -1,58 +1,60 @@
-const formulario = document.getElementById('form-register')
+const formRegistro = document.getElementById('form-register')
+const otp = document.querySelector('.codigo-container')
 
-const botao = document.getElementById('codigo-button')
+formRegistro.addEventListener('submit', async function enviar_registro(event) {
+    event.preventDefault()
 
-const codigo_container = document.querySelector('.codigo-container')
-let email = ""
+    const rawdados = new FormData(formRegistro)
+    const Nformulario = Object.fromEntries(rawdados)
 
-formulario.addEventListener('submit', function(e) {
-    e.preventDefault()
-
-    const rawdados = new FormData(formulario)
-    const dados = Object.fromEntries(rawdados)
-    email = String(dados['email'])
-
-    fetch('http://127.0.0.1:5000/api/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dados)
-    })
-    .then(res => res.json())
-    .then(dados => {
-        if (dados['stats_conta'] == '0') {
-            alert('Conta já existente')
-        } else {
-            console.log("Pelo menos a conta não existe -1 problema")
-            codigo_container.style.display = 'flex'
-        }
-    })
-})
-
-botao.addEventListener('click', function() {
-    const input = document.getElementById('codigo-input')
-    const num = input.value
-
-    if(Number.isFinite(Number(num)) && String(num).length == "6") {
-        fetch('http://127.0.0.1:5000/api/codigo', {
+    try {
+        const resposta = await fetch('http://127.0.0.1:5000/api/registro', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({'codigo': num, 'email': email})
+            body: JSON.stringify(Nformulario)
         })
-        .then(res => res.json())
-        .then(dado => {
-            if (dado['stats_conta'] == "2") {
-                alert('codigo certo')
-            } else if (dado['stats_conta'] == "3") {
-                alert("errado")
-            } else if (dado['stats_conta'] == "4") {
-                alert("Senha invalida")
-            }
+
+        const dados = await resposta.json()
+        console.log(dados)
+
+        if (dados['codigo'] == '1') {
+            alert('Esse email já existe')
+            window.location.href = '/pages/login'
+        }
+        // Abrir o codigo de email, OTP
+        if (dados['status'] == '200') {
+            otp.style.display = 'flex'
+        } else {
+            console.error(dados)
+        }
+    }
+    catch(e) {
+        console.error(e)
+    }
+})
+
+const btn_reenviar = document.getElementById('codigo-button')
+
+btn_reenviar.addEventListener('click', async function() {
+    const inp_reenviar = document.getElementById('codigo-input')
+
+    const rawdados = new FormData(formRegistro)
+    const formulario = Object.fromEntries(rawdados)
+    
+    try {
+        const resposta = await fetch('http://127.0.0.1:5000/api/codigo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'codigo': inp_reenviar.value, 'email': formulario.email})
         })
-    } else{
-        alert("Digite um codigo válido")
+        const dados = await resposta.json()
+
+        alert(dados['status'])
+    } catch (e) {
+        console.error(e)
     }
 })
